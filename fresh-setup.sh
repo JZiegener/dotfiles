800eacd1c049f45c5921af5fd6d2b6d6e7a99ff1
Jeff <JZiegener@gmail.com>
2023-11-04 20:29:37 -0400

    Fix typos

--- fresh-setup.sh
+++ fresh-setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+ssh-keygen -t ed25519 -C "JZiegener@gmail.com" -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+ssh-add ~/.ssh/id_ed25519
+
 sudo apt-get install git -y
-mkdr ~/repos
+mkdir ~/repos
 cd repos 
 git clone https://github.com/JZiegener/dotfiles.git
 cd dotfiles
@@ -9,5 +12,3 @@ sudo ./install/install-base-system.sh
 cp .bashrc ~/.bashrc
 source ~/.bashrc
 
-ssh-keygen -t ed25519 -C "JZiegener@gmail.com"
-ssh-add ~/.ssh/id_ed25519
