ab71406761d9f672028f9db7dd93406f32f0e1e4
Jeff Ziegener <JZiegener@gmail.com>
2023-11-04 22:07:18 -0400

    Add drivers, flatpak config

--- install/install-base-system.sh
+++ install/install-base-system.sh
@@ -1,26 +1,37 @@
 #!/bin/sh
 
 sudo apt-get update
+sudo apt-get install nvidia-driver
 sudo apt-get install flatpak
+if ($XDG_CURRENT_DESKTOP == "GNOME")
+    sudo apt-get install gnome-software-plugin-flatpak
+fi
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
 sudo apt-get install nfs-common
 sudo apt-get install lm-sensors
 sudo apt-get install htop
 sudo apt-get install powertop
 sudo apt-get install vim
+cp ~/repos/dotfiles/vim/.vimrc ~/.vimrc
 sudo apt-get install curl
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt-get install ansible
 #Other repos
 #VS Vodium
-echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main'     | sudo tee /etc/apt/sources.list.d/vscodium.list
+flatpak install flathub vscodium -y
 #Spotify
-curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client
+flatpak install flathub spotify -y
+
 sudo apt-get install tmux
 cp ~/repos/dotfiles/tmux/tmux.conf ~
-flatpack install firefox
+flatpak install flathub firefox
+
 sudo mkdir -p /etc/firefox/policies
 sudo cp ~/repos/dotfiles/install/config/firefox-policies.json /etc/firefox/policies/policies.json
+
+git config --global user.email "JZiegener@gmail.com"
+git config --global user.name "Jeff Ziegener"
+config --global core.editor "vim"
